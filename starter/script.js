7af13d8e498402132993c2710cd82d5b6fda1afd
Salim Al Aamri <salimalaamri@gmail.com>
2025-09-03 13:15:19 +0400

    practice inheritence classes es6

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,26 @@
 // 'use strict';
 
-// class Person {
-//   constructor(firstName, birthYear) {
-//     this.firstName = firstName;
-//     this.birthYear = birthYear;
-//   }
-// }
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+}
+
+class Student extends Person {
+  constructor(firstName,birthYear){
+    super(firstName);
+    this.birthYear = birthYear
+  }
+  introduce(){
+    console.log(`My name is ${this.firstName}, I was born ${this.birthYear}`);
+  }
+}
+
+const sami = new Student('Sami Said', 1992);
+
+sami.introduce()
+
 
 // const salim = new Person('Salim', 1990);
 // console.log(salim);
@@ -78,6 +93,8 @@ class Device {
   }
 }
 
+
+
 const ping = new Device('GH');
 // ping.randomIP();
 
@@ -95,5 +112,20 @@ this.birthYear=birthYear;
 }
 
 const salim = Object.create(PersonProto);
-salim.init('salim',1990);
-salim.calcAge()
+// salim.init('salim',1990);
+// salim.calcAge()
+
+//todo: >>>>>>         Challenge         
+// Coding Challenge #3
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
